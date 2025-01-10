181eb7e468e3b72f1084b09b0259a1f4e77bbe56
Daniela Wiepert <daniela.wiepert7@gmail.com>
2025-01-10 02:08:37 -0600

    support for flac

--- audio_preprocessing/_version.py
+++ audio_preprocessing/_version.py
@@ -1,7 +1,8 @@
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 #change log:
 # 0.0.0: first version
 # 0.0.1: stimulus loading/processingg
 # 0.0.2: added windowing transform
 # 0.0.3: added stimuli selection
+# 0.0.4: support for flac
